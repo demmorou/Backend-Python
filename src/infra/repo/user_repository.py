a6accc7038a7513f141d42bdb82d95f8068ec14c
Deusimar <sousadeusimar2@gmail.com>
2021-05-27 08:32:38 -0300

    feat(user): added methods find by id and find by name

--- src/infra/repo/user_repository.py
+++ src/infra/repo/user_repository.py
@@ -2,7 +2,7 @@
 
 from src.domain.models import Users
 from src.infra.config import DatabaseConnectionHandler
-from src.infra.entities import Users as UsersModels
+from src.infra.entities import Users as UsersModel
 
 
 class UserRepository:
@@ -18,13 +18,51 @@ class UserRepository:
 
         with DatabaseConnectionHandler() as db_connection:
             try:
-                new_user = UsersModels(name=name, password=password)
+                new_user = UsersModel(name=name, password=password)
                 db_connection.session.add(new_user)
                 db_connection.session.commit()
 
                 return Users(
                     id=new_user.id, name=new_user.name, password=new_user.password
                 )
+            except:
+                db_connection.session.rollback()
+                raise
+            finally:
+                db_connection.session.close()
+
+    @classmethod
+    def find_by_name(cls, name: str) -> Users:
+        """Find an User by your name
+        :param - name: User's name
+        :return - Returns an user
+        """
+
+        with DatabaseConnectionHandler() as db_connection:
+            try:
+                user = (
+                    db_connection.session.query(UsersModel).filter_by(name=name).one()
+                )
+                return user
+            except:
+                db_connection.session.rollback()
+                raise
+            finally:
+                db_connection.session.close()
+
+    @classmethod
+    def find_by_id(cls, user_id: str) -> Users:
+        """Find an User by your id
+        :param - user_id: User's id
+        :return - Returns an user
+        """
+
+        with DatabaseConnectionHandler() as db_connection:
+            try:
+                user = (
+                    db_connection.session.query(UsersModel).filter_by(id=user_id).one()
+                )
+                return user
             except:
                 db_connection.session.rollback()
                 raise
--- src/infra/repo/user_repository_test.py
+++ src/infra/repo/user_repository_test.py
@@ -23,3 +23,33 @@ def test_insert_user():
     assert new_user.id == user.id
     assert new_user.name == user.name
     assert new_user.password == user.password
+
+
+def test_find_by_name():
+    """Should be able return an user by your name"""
+
+    name = faker.name()
+    password = faker.word()
+    user = user_repository.insert_user(name=name, password=password)
+
+    created_user = user_repository.find_by_name(name=name)
+
+    assert user == created_user
+
+    engine = db_connection_handler.get_engine()
+    engine.execute(f"DELETE FROM users WHERE id='{user.id}';")
+
+
+def test_find_by_id():
+    """Should be able return an user by your id"""
+
+    name = faker.name()
+    password = faker.word()
+    user = user_repository.insert_user(name=name, password=password)
+
+    created_user = user_repository.find_by_id(user_id=user.id)
+
+    assert user == created_user
+
+    engine = db_connection_handler.get_engine()
+    engine.execute(f"DELETE FROM users WHERE id='{user.id}';")
