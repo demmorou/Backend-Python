d124f6c9e8f5a9fb0ff7649ff54190907857d5bc
Deusimar <sousadeusimar2@gmail.com>
2021-05-27 11:05:11 -0300

    feat(pets): added methos to find pets by user_id, name and id

--- src/infra/repo/pet_repository.py
+++ src/infra/repo/pet_repository.py
@@ -1,5 +1,6 @@
 # pylint: disable=E1101
 
+from typing import List
 from src.domain.models import Pets
 from src.infra.config import DatabaseConnectionHandler
 from src.infra.entities import Pets as PetsModel
@@ -32,6 +33,76 @@ class PetRepository:
                     age=pet.age,
                     user_id=pet.user_id,
                 )
+            except:
+                db_connection.session.rollback()
+                raise
+            finally:
+                db_connection.session.close()
+
+    @classmethod
+    def find_by_id(cls, pet_id: int) -> Pets:
+        """Return a pet by you id
+        :param - pet_id: Pet's id
+        return: Returns a pet
+        """
+
+        with DatabaseConnectionHandler() as db_connection:
+            try:
+                pet = db_connection.session.query(PetsModel).filter_by(id=pet_id).one()
+
+                return PetsModel(
+                    id=pet.id,
+                    name=pet.name,
+                    specie=pet.specie.value,
+                    age=pet.age,
+                    user_id=pet.user_id,
+                )
+            except:
+                db_connection.session.rollback()
+                raise
+            finally:
+                db_connection.session.close()
+
+    @classmethod
+    def find_by_name(cls, name: str) -> Pets:
+        """Return a pet by you name
+        :param - name: Pet's name
+        return: Returns a pet
+        """
+
+        with DatabaseConnectionHandler() as db_connection:
+            try:
+                pet = db_connection.session.query(PetsModel).filter_by(name=name).one()
+
+                return PetsModel(
+                    id=pet.id,
+                    name=pet.name,
+                    specie=pet.specie.value,
+                    age=pet.age,
+                    user_id=pet.user_id,
+                )
+            except:
+                db_connection.session.rollback()
+                raise
+            finally:
+                db_connection.session.close()
+
+    @classmethod
+    def find_by_user_id(cls, user_id: int) -> List[Pets]:
+        """Return a list of pets by user_id
+        :param - user_id: Pet's user_id
+        return: Returns a list with pets selecteds
+        """
+
+        with DatabaseConnectionHandler() as db_connection:
+            try:
+                pets = (
+                    db_connection.session.query(PetsModel)
+                    .filter_by(user_id=user_id)
+                    .all()
+                )
+
+                return pets
             except:
                 db_connection.session.rollback()
                 raise
--- src/infra/repo/pet_repository_test.py
+++ src/infra/repo/pet_repository_test.py
@@ -1,6 +1,6 @@
 from faker import Faker
 from src.infra.config import DatabaseConnectionHandler
-from src.infra.entities import AnimalTypes
+from src.infra.entities import AnimalTypes, Pets
 from .pet_repository import PetRepository
 
 faker = Faker()
@@ -12,7 +12,7 @@ def test_create_pet():
     """Should be able create a new pet"""
 
     name = faker.name()
-    specie = AnimalTypes.cat
+    specie = AnimalTypes("cat")
     age = faker.random_number(digits=1)
     user_id = faker.random_number()
 
@@ -27,3 +27,73 @@ def test_create_pet():
     assert pet.age == created_pet.age
 
     engine.execute(f"DELETE FROM pets WHERE id='{pet.id}';")
+
+
+def test_find_by_id():
+    """Should be able return by your id"""
+
+    name = faker.name()
+    specie = AnimalTypes("cat")
+    age = faker.random_number(digits=1)
+    user_id = faker.random_number()
+
+    pet = pet_repository.create(name, specie, age, user_id)
+
+    engine = db_connection_handler.get_engine()
+    created_pet = pet_repository.find_by_id(pet_id=pet.id)
+
+    assert pet.id == created_pet.id
+    assert pet.name == created_pet.name
+    assert pet.specie == created_pet.specie
+    assert pet.age == created_pet.age
+
+    engine.execute(f"DELETE FROM pets WHERE id='{pet.id}';")
+
+
+def test_find_by_name():
+    """Should be able return by your name"""
+
+    name = faker.name()
+    specie = AnimalTypes("cat")
+    age = faker.random_number(digits=1)
+    user_id = faker.random_number()
+
+    pet = pet_repository.create(name, specie, age, user_id)
+
+    engine = db_connection_handler.get_engine()
+    created_pet = pet_repository.find_by_name(name=name)
+
+    assert pet.id == created_pet.id
+    assert pet.name == created_pet.name
+    assert pet.specie == created_pet.specie
+    assert pet.age == created_pet.age
+
+    engine.execute(f"DELETE FROM pets WHERE id='{pet.id}';")
+
+
+def test_find_by_user_id():
+    """Should be able return by your name"""
+
+    pet_id = faker.random_number(digits=4)
+    name = faker.name()
+    specie = "dog"
+    age = faker.random_number(digits=1)
+    user_id = faker.random_number(digits=5)
+
+    specie_mock = AnimalTypes("dog")
+
+    data = Pets(id=pet_id, name=name, specie=specie_mock, age=age, user_id=user_id)
+
+    engine = db_connection_handler.get_engine()
+
+    engine.execute(
+        "INSERT INTO pets (id, name, specie, age, user_id) VALUES ('{}', '{}', '{}', '{}', '{}');".format(
+            pet_id, name, specie, age, user_id
+        )
+    )
+
+    pets = pet_repository.find_by_user_id(user_id=user_id)
+
+    assert data in pets
+
+    engine.execute(f"DELETE FROM pets WHERE id='{pet_id}';")
